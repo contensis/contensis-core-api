1e8b57ec1fd199eb74c420bd2564540087e533be
Neil Flatley <n.flatley@zengenti.com>
2024-02-28 11:42:52 +0000

    style: fix new whitespace inconsistencies

--- src/models/search/ManagementQuery.ts
+++ src/models/search/ManagementQuery.ts
@@ -4,34 +4,34 @@ import { WhereExpression } from './Operators';
 import { serializeOrder } from './QueryTypes';
 
 export class ManagementQuery
-	implements Omit<ContensisQuery, 'fields' | 'fieldLinkDepths'>
+    implements Omit<ContensisQuery, 'fields' | 'fieldLinkDepths'>
 {
-	where: WhereExpression = new WhereExpression();
-	orderBy: string | string[] | ContensisQueryOrderBy = [];
-	pageIndex: number = 0;
-	pageSize: number = 20;
-	includeArchived?: boolean = false;
-	includeDeleted?: boolean = false;
+    where: WhereExpression = new WhereExpression();
+    orderBy: string | string[] | ContensisQueryOrderBy = [];
+    pageIndex: number = 0;
+    pageSize: number = 20;
+    includeArchived?: boolean = false;
+    includeDeleted?: boolean = false;
 
-	constructor(...whereExpressions: IExpression[]) {
-		if (whereExpressions) {
-			this.where.addRange(whereExpressions);
-		}
-	}
+    constructor(...whereExpressions: IExpression[]) {
+        if (whereExpressions) {
+            this.where.addRange(whereExpressions);
+        }
+    }
 
-	toJSON() {
-		let result: any = {};
-		result.pageIndex = this.pageIndex;
-		result.pageSize = this.pageSize;
+    toJSON() {
+        let result: any = {};
+        result.pageIndex = this.pageIndex;
+        result.pageSize = this.pageSize;
 
-		let orderByDtos = serializeOrder(this.orderBy);
-		if (orderByDtos && orderByDtos.length > 0) {
-			result.orderBy = orderByDtos;
-		}
+        let orderByDtos = serializeOrder(this.orderBy);
+        if (orderByDtos && orderByDtos.length > 0) {
+            result.orderBy = orderByDtos;
+        }
 
-		result.where = this.where;
-		result.includeArchived = this.includeArchived;
-		result.includeDeleted = this.includeDeleted;
-		return result;
-	}
+        result.where = this.where;
+        result.includeArchived = this.includeArchived;
+        result.includeDeleted = this.includeDeleted;
+        return result;
+    }
 }
